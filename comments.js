121eda7e6cf6201194eb3e211373808827a5c571
Hari2150423 <harikrishna.j2@cognizant.com>
2025-02-06 15:33:33 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,38 @@
 //create the web server
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var qs = require('querystring');
-var comments = require('./comments.json');
-
+const express = require('express');
+const app = express();
 //create the server
-http.createServer(function (req, res) {
-	//parse the url
-	var pathname = url.parse(req.url).pathname;
-	//parse the query
-	var query = url.parse(req.url).query;
+const server = require('http').Server(app);
+//create the web socket
+const io = require('socket.io')(server);
+//create the database
+const mongoose = require('mongoose');
+//connect to the database
+mongoose.connect('mongodb://localhost:27017/comments', { useNewUrlParser: true });
+
+const Comment = mongoose.model('Comment', {
+    name: String,
+    comment: String
+});
+
+app.use(express.static('public'));
+
+app.get('/comments', async (req, res) => {
+    const comments = await Comment.find();
+    res.send(comments);
+});
+
+app.post('/comments', async (req, res) => {
+    const comment = new Comment(req.body);
+    await comment.save();
+    io.emit('comment', req.body);
+    res.sendStatus(201);
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+server.listen(3000, () => {
+    console.log('listening on http://localhost:3000');
+});
