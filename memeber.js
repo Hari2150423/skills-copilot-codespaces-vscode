782f388aa3f1b59e85ba9126933c66e506817680
Hari2150423 <harikrishna.j2@cognizant.com>
2025-02-06 15:03:54 +0000

    copilot 2nd

---
+++ memeber.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    let member = {
+        name: "Jhon",
+        age: 25,
+        skills: ["Javascript", "React", "Nodejs"],
+        address: {
+            city: "New York",
+            country: "USA"
+        }
+    }
+
+    return member;
+}
