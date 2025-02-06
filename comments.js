82ae1635fa516da3744d521180197614619b3a3b
Hari2150423 <harikrishna.j2@cognizant.com>
2025-02-06 15:37:03 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,38 +1,4 @@
 //create the web server
-const express = require('express');
-const app = express();
-//create the server
-const server = require('http').Server(app);
-//create the web socket
-const io = require('socket.io')(server);
-//create the database
-const mongoose = require('mongoose');
-//connect to the database
-mongoose.connect('mongodb://localhost:27017/comments', { useNewUrlParser: true });
-
-const Comment = mongoose.model('Comment', {
-    name: String,
-    comment: String
-});
-
-app.use(express.static('public'));
-
-app.get('/comments', async (req, res) => {
-    const comments = await Comment.find();
-    res.send(comments);
-});
-
-app.post('/comments', async (req, res) => {
-    const comment = new Comment(req.body);
-    await comment.save();
-    io.emit('comment', req.body);
-    res.sendStatus(201);
-});
-
-io.on('connection', (socket) => {
-    console.log('a user connected');
-});
-
-server.listen(3000, () => {
-    console.log('listening on http://localhost:3000');
-});
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
