f1f7d2299f623bbb49b52d3a45b024438d285774
Hari2150423 <harikrishna.j2@cognizant.com>
2025-02-06 15:31:20 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,32 +1,13 @@
 //create the web server
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const bodyParser = require('body-parser');
-const app = express();
-const port = 3000;
-const commentsPath = path.join(__dirname, 'comments.json');
-const comments = require(commentsPath);
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var qs = require('querystring');
+var comments = require('./comments.json');
 
-app.use(bodyParser.json());
-app.use(express.static('public'));
-
-app.get('/comments', (req, res) => {
-    res.json(comments);
-});
-
-app.post('/comments', (req, res) => {
-    const newComment = req.body;
-    comments.push(newComment);
-    fs.writeFile(commentsPath, JSON.stringify(comments), (err) => {
-        if (err) {
-            res.status(500).send('Could not write to file');
-        } else {
-            res.status(201).send('Comment added');
-        }
-    });
-});
-
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
+//create the server
+http.createServer(function (req, res) {
+	//parse the url
+	var pathname = url.parse(req.url).pathname;
+	//parse the query
+	var query = url.parse(req.url).query;
